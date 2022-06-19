a161965410db6f7f493960922b3b0ac0f0c84113
luqlap <luqlap@example.com>
2022-06-19 16:43:52 +0200

    build(eslint): add eslint support

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "preact/compat";
+import ReactDOM from "react-dom";
 
 import App from "./App";
 import "./index.css";
 
-React.render(<App />, document.body);
+ReactDOM.render((<App />) as any, document.body);
